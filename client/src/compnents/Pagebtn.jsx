14cce546f521d43accf5b43e9dfaa05c9dfd399a
Shahmeer-09 <shahmeerweb@gmail.com>
2024-04-15 18:35:19 +0500

    again pagination

--- client/src/compnents/Pagebtn.jsx
+++ client/src/compnents/Pagebtn.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Pagination from "@mui/material/Pagination";
+import Pagination from '@mui/material/Pagination';
 
 import { useLocation, useNavigate } from "react-router-dom";
 import { AlljobsContext } from "../pages/AllJobs";
@@ -8,29 +8,26 @@ import Wrapper from "../assets/wrappers/PageBtnContainer";
 const PageBtnContainer = () => {
   const {data} = React.useContext(AlljobsContext);
   const { numOfPages, currentpage } = data;
-  const [page, setpage]= useState(currentpage)
   const { search, pathname } = useLocation();
   const navigate = useNavigate();
 
-  const handlePageChange = ( event,pageNumber) => {
-    setpage(pageNumber)
-  };
-  useEffect(()=>{
+  const handlePageUpdate =(event, pageNumber)=>{
     const searchParams = new URLSearchParams(search);
-    searchParams.set("page",page );
+    searchParams.set("page",pageNumber );
     navigate(`${pathname}?${searchParams.toString()}`);
-  },[])
+  }
+
 
   return (
-    <Wrapper>
+  
       <Pagination
         count={numOfPages}
-        page={page}
-        onChange={handlePageChange}
+        page={currentpage}
+        onChange={handlePageUpdate}
         variant="outlined"
         shape="rounded"
       />
-    </Wrapper>
+  
   );
 };
 
