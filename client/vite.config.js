57ab2fa4f1f6e69340a0f3839e14159ea2ac14e7
Shahmeer-09 <shahmeerweb@gmail.com>
2024-04-15 19:57:41 +0500

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: "https://job-find-mern.vercel.app/",
-        secure: false
+        target: "https://job-find-mern.vercel.app",
+        secure: true
       }
     }
   }
